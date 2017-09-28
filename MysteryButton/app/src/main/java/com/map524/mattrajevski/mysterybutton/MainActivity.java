f3864153fc0c84f6e29d01b9d8fe6f557b8eb33c
Matt <32369912+mrajevski@users.noreply.github.com>
2017-09-28 15:41:50 -0400

    no

---
+++ MysteryButton/app/src/main/java/com/map524/mattrajevski/mysterybutton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.map524.mattrajevski.mysterybutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
