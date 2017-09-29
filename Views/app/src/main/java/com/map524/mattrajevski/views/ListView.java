ef590b8e08f5588a1fe0adeac72701e58b4ddb6c
Matt <32369912+mrajevski@users.noreply.github.com>
2017-09-29 12:05:43 -0400

    Views Example

---
+++ Views/app/src/main/java/com/map524/mattrajevski/views/ListView.java
@@ -0,0 +1,13 @@
+package com.map524.mattrajevski.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
